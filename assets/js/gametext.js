feedef565fc36665383ed5fd30372a19ea6aeea9
JamesWorsnop <james.worsnop+github@googlemail.com>
2016-07-01 21:41:06 +0900

    Added hover JS and CSS that closes #1

---
+++ assets/js/gametext.js
@@ -0,0 +1,46 @@
+var game_brief = {};
+game_brief["sonic1"] = "Sonic the Hedgehog (Japanese: ソニック・ザ・ヘッジホッグ \
+  Hepburn: Sonikku za Hejjihoggu?) is a platform video game developed by Sonic \
+  Team and published by Sega for the Sega Genesis console. The game was first \
+  released in North America, Europe and Australia on June 23, 1991, and in \
+  Japan the following month. The game features an anthropomorphic hedgehog \
+  named Sonic in a quest to defeat Dr. Robotnik, a scientist who has \
+  imprisoned animals in robots and stolen the magical Chaos Emeralds. \
+  Sonic the Hedgehog's gameplay involves collecting rings as a form of \
+  health and a simple control scheme, with jumping and attacking controlled \
+  by a single button."
+
+game_brief["sonic2"] = "Sonic the Hedgehog 2 (Japanese: ソニック・ザ・\
+  ヘッジホッグ2ツー Hepburn: Sonikku za Hejjihoggu Tsū?) is a platform video game \
+  developed by Sonic Team, with assistance from Sega Technical Institute, and \
+  published by Sega for the Sega Genesis console. It was released worldwide in \
+  November 1992. The game is the second main entry in the Sonic the Hedgehog \
+  series, and the first to feature Miles 'Tails' Prower, a flying two-tailed \
+  fox. He and protagonist Sonic the Hedgehog must stop the series antagonist \
+  Dr. Ivo Robotnik from stealing the Chaos Emeralds in order to power his \
+  space station, called the Death Egg."
+
+game_brief["sonic3"] = "Sonic the Hedgehog 3 (Japanese: ソニック・ザ・ヘッジホッグ\
+  3スリー Hepburn: Sonikku za Hejjihoggu Surī?) is a platform video game \
+  developed by Sonic Team and published by Sega. The third main game in the \
+  Sonic the Hedgehog series, it was released for the Sega Genesis worldwide \
+  in February 1994, and in Japan three months later. Following the events of \
+  Sonic the Hedgehog 2, Dr. Robotnik's spaceship, the Death Egg, crash-lands \
+  on a mysterious floating island. There, Sonic and Tails must once more \
+  retrieve the Chaos Emeralds to stop Death Egg from relaunching, while \
+  making rounds with the island's guardian, Knuckles the Echidna."
+
+game_brief["s3andk"] = "Sonic & Knuckles (Japanese: ソニック&ナックルズ Hepburn: \
+  Sonikku to Nakkuruzu?) is a platform video game developed by Sonic Team and \
+  published by Sega for the Sega Genesis. Released on October 18, 1994, it was \
+  the fourth installment in the main Sonic the Hedgehog series. The game is a \
+  direct sequel to Sonic the Hedgehog 3, and follows both Sonic the Hedgehog \
+  and Knuckles the Echidna in their respective quests to save Angel Island; \
+  Sonic tries to prevent Dr. Robotnik from re-launching his orbital weapon, \
+  the Death Egg, while Knuckles scuffles with Robotnik's minion, EggRobo."
+
+var game_titles = {};
+game_titles['sonic1'] = "Sonic The Hedgehog"
+game_titles['sonic2'] = "Sonic The Hedgehog 2"
+game_titles['sonic3'] = "Sonic The Hedgehog 3"
+game_titles['s3andk'] = "Sonic The Hedgehog and Knuckles"
---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $(".game-image").hover(
+    function(){
+      console.log($(this).attr("id"));
+      $("#game-info-title").text(game_titles[$(this).attr("id")]);
+      $("#game-info-text").text(game_brief[$(this).attr("id")]);
+      $("#game-info").addClass("lightSpeedIn");
+      $("#game-info").removeClass("lightSpeedOut");
+    }, function(){
+      $("#game-info").addClass("lightSpeedOut");
+      $("#game-info").removeClass("lightSpeedIn");
+      }
+    );
+});
