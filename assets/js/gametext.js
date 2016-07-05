054bcc7b711c476ed007d21ae6ae8e8580a9ff26
JamesWorsnop <James@tsurusonomiho-no-MacBook-Pro.local>
2016-07-06 07:48:32 +0900

    Tidied up HTML, reduced image size, added more games, made more mobile responsive

--- assets/js/gametext.js
+++ assets/js/gametext.js
@@ -1,5 +1,5 @@
-var game_brief = {};
-game_brief["sonic1"] = "Sonic the Hedgehog (Japanese: ソニック・ザ・ヘッジホッグ \
+var game_brief = [];
+game_brief["0"] = "Sonic the Hedgehog (Japanese: ソニック・ザ・ヘッジホッグ \
   Hepburn: Sonikku za Hejjihoggu?) is a platform video game developed by Sonic \
   Team and published by Sega for the Sega Genesis console. The game was first \
   released in North America, Europe and Australia on June 23, 1991, and in \
@@ -10,7 +10,7 @@ game_brief["sonic1"] = "Sonic the Hedgehog (Japanese: ソニック・ザ・ヘ�
   health and a simple control scheme, with jumping and attacking controlled \
   by a single button."
 
-game_brief["sonic2"] = "Sonic the Hedgehog 2 (Japanese: ソニック・ザ・\
+game_brief["1"] = "Sonic the Hedgehog 2 (Japanese: ソニック・ザ・\
   ヘッジホッグ2ツー Hepburn: Sonikku za Hejjihoggu Tsū?) is a platform video game \
   developed by Sonic Team, with assistance from Sega Technical Institute, and \
   published by Sega for the Sega Genesis console. It was released worldwide in \
@@ -20,7 +20,16 @@ game_brief["sonic2"] = "Sonic the Hedgehog 2 (Japanese: ソニック・ザ・\
   Dr. Ivo Robotnik from stealing the Chaos Emeralds in order to power his \
   space station, called the Death Egg."
 
-game_brief["sonic3"] = "Sonic the Hedgehog 3 (Japanese: ソニック・ザ・ヘッジホッグ\
+game_brief["2"] = "Sonic the Hedgehog CD (ソニック・ザ・ヘッジホッグCD Sonikku \
+    za Hejjihoggu Shī Dī?), or Sonic CD (ソニックCD Sonikku Shī Dī?), is a \
+    platform video game developed by Sonic Team and published by Sega \
+    originally for the Sega CD in 1993. It is the first game in the \
+    Sonic series to make use of the CD-ROM format, featuring high quality \
+    audio and full motion video. Development began in Japan around the same \
+    time as Sonic the Hedgehog 2 in the United States. Originally, the games \
+    had many similarities, but over time became vastly different projects."
+
+game_brief["3"] = "Sonic the Hedgehog 3 (Japanese: ソニック・ザ・ヘッジホッグ\
   3スリー Hepburn: Sonikku za Hejjihoggu Surī?) is a platform video game \
   developed by Sonic Team and published by Sega. The third main game in the \
   Sonic the Hedgehog series, it was released for the Sega Genesis worldwide \
@@ -30,7 +39,7 @@ game_brief["sonic3"] = "Sonic the Hedgehog 3 (Japanese: ソニック・ザ・ヘ
   retrieve the Chaos Emeralds to stop Death Egg from relaunching, while \
   making rounds with the island's guardian, Knuckles the Echidna."
 
-game_brief["s3andk"] = "Sonic & Knuckles (Japanese: ソニック&ナックルズ Hepburn: \
+game_brief["4"] = "Sonic & Knuckles (Japanese: ソニック&ナックルズ Hepburn: \
   Sonikku to Nakkuruzu?) is a platform video game developed by Sonic Team and \
   published by Sega for the Sega Genesis. Released on October 18, 1994, it was \
   the fourth installment in the main Sonic the Hedgehog series. The game is a \
@@ -39,8 +48,68 @@ game_brief["s3andk"] = "Sonic & Knuckles (Japanese: ソニック&ナックルズ
   Sonic tries to prevent Dr. Robotnik from re-launching his orbital weapon, \
   the Death Egg, while Knuckles scuffles with Robotnik's minion, EggRobo."
 
-var game_titles = {};
-game_titles['sonic1'] = "Sonic The Hedgehog"
-game_titles['sonic2'] = "Sonic The Hedgehog 2"
-game_titles['sonic3'] = "Sonic The Hedgehog 3"
-game_titles['s3andk'] = "Sonic The Hedgehog and Knuckles"
+game_brief["5"] = "Sonic 3D Blast (Japanese: ソニック３Ｄブラスト Hepburn: \
+  Sonikku Surīdī Burasuto?), also known as Sonic 3D: Flickies' \
+  Island (ソニック３Ｄ フリッキーアイランド Sonikku Surīdī Furikkī Airando?), \
+  is a 1996 platform video game developed by Traveller's Tales and Sonic Team, \
+  and published by Sega for the Sega Genesis, Sega Saturn, and Microsoft \
+  Windows. Part of the Sonic the Hedgehog series, the game abandons previous \
+  Sonic games' side-scrolling style in favor of isometric gameplay, making \
+  use of some pre-rendered 3D models converted into sprites. The game \
+  features creatures known as Flickies, which first appeared in the 1984 \
+  arcade game Flicky. Controlling only Sonic himself, the player's goal is to \
+  collect Flickies and carry them to safety, periodically sparring with the \
+  series antagonist Dr. Robotnik, who is imprisoning them within robots."
+
+game_brief["6"] = "Sonic R (ソニックＲ Sonikku Āru?) is a racing video game \
+  developed by Traveller's Tales and Sonic Team and published by Sega for the \
+  Sega Saturn and Microsoft Windows. The game was released in North America on \
+  November 18, 1997, in Japan on December 4, Europe and Australia on December 11. \
+  It was re-released for the PlayStation 2 and Nintendo GameCube as part of the \
+  Sonic Gems Collection. Sonic R is the third Sonic racing game and the last of \
+  two Sonic games to be co-developed by Traveller's Tales and Sonic Team. In the \
+  game, players can take control of one of ten Sonic series characters and \
+  participate in races on various Sonic-themed race tracks. The game's story \
+  focuses the characters' efforts to win a series of races and prevent Doctor \
+  Robotnik from stealing the Chaos Emeralds and enslaving the world."
+
+game_brief["7"] = "Sonic Adventure (ソニックアドベンチャー Sonikku Adobenchā?) is \
+  a platform video game developed by Sonic Team and published by Sega. It was \
+  originally released for the Dreamcast as a launch title in Japan in 1998, \
+  and worldwide in 1999. It was the first game in the main Sonic the Hedgehog \
+  series since Sonic & Knuckles in 1994, and was the best-selling game on the \
+  Dreamcast. After series villain Doctor Eggman releases an ancient monster, \
+  protagonist Sonic the Hedgehog vows to stop him collecting the Chaos \
+  Emeralds. The other playable characters are flying fox Miles 'Tails' Prower, \
+  love interest Amy Rose, Knuckles the Echidna, who plans to reassemble the \
+  Master Emerald, angler Big the Cat, and E-102 Gamma, one of Eggman's robots."
+
+var game_titles = [];
+game_titles['0'] = "Sonic The Hedgehog"
+game_titles['1'] = "Sonic The Hedgehog 2"
+game_titles['2'] = "Sonic The Hedgehog CD"
+game_titles['3'] = "Sonic The Hedgehog 3"
+game_titles['4'] = "Sonic The Hedgehog and Knuckles"
+game_titles['5'] = "Sonic 3D Blast"
+game_titles['6'] = "Sonic R"
+game_titles['7'] = "Sonic Adventure"
+
+var game_release_year = [];
+game_release_year['0'] = "1991"
+game_release_year['1'] = "1992"
+game_release_year['2'] = "1993"
+game_release_year['3'] = "1994"
+game_release_year['4'] = "1994"
+game_release_year['5'] = "1996"
+game_release_year['6'] = "1997"
+game_release_year['7'] = "1999"
+
+var game_links = [];
+game_links['0'] = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_(1991_video_game)"
+game_links['1'] = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_2"
+game_links['2'] = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_CD"
+game_links['3'] = "https://en.wikipedia.org/wiki/Sonic_the_Hedgehog_3"
+game_links['4'] = "https://en.wikipedia.org/wiki/Sonic_%26_Knuckles"
+game_links['5'] = "https://en.wikipedia.org/wiki/Sonic_3D_Blast"
+game_links['6'] = "https://en.wikipedia.org/wiki/Sonic_R"
+game_links['7'] = "https://en.wikipedia.org/wiki/Sonic_Adventure"
